df21289a031507ab017295f88e72b7414a2276bd
MoeskerDev <mgmoesker@gmail.com>
2021-11-24 20:56:13 +0000

    add py script file and requirements file

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,3 +1,25 @@
+"""
+File for scripts
+"""
 import os
 import pymysql
 
+# Get username from workspace
+username = os.getenv('USERNAME')
+
+# Connect to the database
+connection =pymysql.connect(host='localhost',
+                            user=username,
+                            password='',
+                            db='Chinook')
+
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    # Always close the connection
+    connection.close()
